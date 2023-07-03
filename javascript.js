a4146fa39fe4f475527ad7fa6ac64d7f00f1ca8d
Max <57727195+mafxs@users.noreply.github.com>
2023-07-03 22:52:22 +0100

    added func playGame, changed playerSelection to func getPlayerChoice

--- javascript.js
+++ javascript.js
@@ -1,22 +1,27 @@
-//function getComputerChoice will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.
+let computerSelection = '';
+let playerSelection = '';
+let winner = '';
+let count = 0;
+
+
+//function getComputerChoice will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’ for the computer choice.
 function getComputerChoice () {
     var selection = ["Rock", "Paper", "Scissors"];
     return selection[Math.floor(Math.random() * selection.length)];     
 }
 
-//computerSelection assigned a RPS value from getComputerChoice.
-let computerSelection = getComputerChoice();
-console.log(computerSelection);
+//function getPlayerChoice will prompt input for RPS value. convert to lower case. convert 1st character to upper case. and return value.
+function getPlayerChoice() {
+    playerSelection = prompt('Play by entering: "Rock", "Paper", or "Scissors"');
+    playerSelection = playerSelection.toLowerCase();
+    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+    return playerSelection;
+}
 
-//playerSelection prompt for RPS value. convert to lower case. convert 1st character to upper case. to fit uniformity in playRound function.
-let playerSelection = prompt('Play by entering: "Rock", "Paper", or "Scissors"');
-playerSelection = playerSelection.toLowerCase();
-playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
-console.log(playerSelection);
-
-//function playRound will play a single round of Rock Paper Scissors, taking parameters of playerSelection and computerSelection
-function playRound (playerSelection, computerSelection) {
-    let winner;
+//function playRound will play a single round of Rock Paper Scissors. playerSelection assigned via getPlayerChoice func, computerSelection assigned via getComputerChoice func.
+function playRound (playerSelection, computerSelection, winner, count) {
+    playerSelection = getPlayerChoice();
+    computerSelection = getComputerChoice();
     if (playerSelection === computerSelection) {
         winner = `It's a draw, both drew ${playerSelection}!`
     } else if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
@@ -31,8 +36,18 @@ function playRound (playerSelection, computerSelection) {
         winner = `Player wins with ${playerSelection} vs Computer's ${computerSelection}!`;
     } else if (playerSelection === 'Paper' && computerSelection === 'Scissors') {
         winner = `Computer wins with ${computerSelection} vs Player's ${playerSelection}!`;
-    } 
-    return winner;
+    } else {
+        winner = 'Typo!';
+    }
+    return count++, winner;
 }
 
-console.log(playRound(playerSelection, computerSelection));
+//playGame will loop through 5 rounds of playRound func.
+function playGame(){
+    for (let i = 0; i<5; i++) {
+        console.log(i);
+        console.log(playRound(playerSelection, computerSelection, winner, count));
+    }
+}
+
+playGame();
