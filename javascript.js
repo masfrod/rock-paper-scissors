0a090923ca9d05f75aec6cf1dc4a4919ad2b5894
Max <57727195+mafxs@users.noreply.github.com>
2023-07-04 21:03:08 +0100

    added playerCount & computerCount counter logic

--- javascript.js
+++ javascript.js
@@ -1,8 +1,8 @@
 let computerSelection = '';
 let playerSelection = '';
 let winner = '';
-let count = 0;
-
+let playerCount = 0;
+let computerCount = 0;
 
 //function getComputerChoice will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’ for the computer choice.
 function getComputerChoice () {
@@ -10,7 +10,7 @@ function getComputerChoice () {
     return selection[Math.floor(Math.random() * selection.length)];     
 }
 
-//function getPlayerChoice will prompt input for RPS value. convert to lower case. convert 1st character to upper case. and return value.
+//function getPlayerChoice will prompt input for RPS value. convert to lower case. convert 1st character to upper case. and return player value.
 function getPlayerChoice() {
     playerSelection = prompt('Play by entering: "Rock", "Paper", or "Scissors"');
     playerSelection = playerSelection.toLowerCase();
@@ -19,34 +19,53 @@ function getPlayerChoice() {
 }
 
 //function playRound will play a single round of Rock Paper Scissors. playerSelection assigned via getPlayerChoice func, computerSelection assigned via getComputerChoice func.
-function playRound (playerSelection, computerSelection, winner, count) {
+function playRound () {
     playerSelection = getPlayerChoice();
     computerSelection = getComputerChoice();
     if (playerSelection === computerSelection) {
         winner = `It's a draw, both drew ${playerSelection}!`
     } else if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
         winner = `Player wins with ${playerSelection} vs Computer's ${computerSelection}!`;
+        playerCount++;
     } else if (playerSelection === 'Rock' && computerSelection === 'Paper') {
         winner = `Computer wins with ${computerSelection} vs Player's ${playerSelection}!`;
+        computerCount++;
     } else if (playerSelection === 'Scissors' && computerSelection === 'Paper') {
         winner = `Player wins with ${playerSelection} vs Computer's ${computerSelection}!`;
+        playerCount++;
     } else if (playerSelection === 'Scissors' && computerSelection === 'Rock') {
         winner = `Computer wins with ${computerSelection} vs Player's ${playerSelection}!`;
+        computerCount++;
     } else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
         winner = `Player wins with ${playerSelection} vs Computer's ${computerSelection}!`;
+        playerCount++;
     } else if (playerSelection === 'Paper' && computerSelection === 'Scissors') {
         winner = `Computer wins with ${computerSelection} vs Player's ${playerSelection}!`;
+        computerCount++;
     } else {
-        winner = 'Typo!';
+        winner = 'There must have beenb a typo here!';
     }
-    return count++, winner;
+    return winner;
 }
 
-//playGame will loop through 5 rounds of playRound func.
+//playGame will loop through 5 rounds of playRound func. winner of round returned and printed. playerCount / computerCount incremented and logged. end of for loop end of game comparison.
 function playGame(){
-    for (let i = 0; i<5; i++) {
-        console.log(i);
-        console.log(playRound(playerSelection, computerSelection, winner, count));
+    for (let i = 1; i<6; i++) {
+        console.log('Round number ' + (i));
+        console.log(playRound());
+        console.log(`Player total wins: ${playerCount}`);
+        console.log(`Computer total wins: ${computerCount}`);
+    }
+    console.log('------------------');
+    console.log('END OF GAME');
+    if (playerCount > computerCount) {
+        console.log('Player won');
+    }
+    if (playerCount < computerCount) {
+        console.log('Computer won');
+    }
+    if (playerCount === computerCount) {
+        console.log('Draw!');
     }
 }
 
